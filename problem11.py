3e699913ac6ba304138348d00c35aab3256b8414
Andrew Watson <awatson@math.utah.edu>
2018-05-09 22:39:16 -0600

    Superficial edits to make the code look a bit cleaner

--- problem11.py
+++ problem11.py
@@ -9,27 +9,27 @@ gStr = grid.read()
 gStr = gStr.split()
 g = [0]*len(gStr)
 
-for i in range(0,len(gStr)):
+for i in range(0, len(gStr)):
     g[i] = int(gStr[i])
 
-maxproduct = 0
+max_product = 0
 
-for i in range(0,20):
-    for j in range(0,20):
-        if (i<17):
+for i in range(0, 20):
+    for j in range(0, 20):
+        if i < 17:
             prod = g[20*i+j]*g[20*(i+1)+j]*g[20*(i+2)+j]*g[20*(i+3)+j]
-            if (prod > maxproduct):
-                maxproduct = prod
-        if (i<17) and (j<17):
+            if prod > max_product:
+                max_product = prod
+        if i < 17 and j < 17:
             prod = g[20*i+j]*g[20*(i+1)+j+1]*g[20*(i+2)+j+2]*g[20*(i+3)+j+3]
-            if (prod > maxproduct):
-                maxproduct = prod
-        if (j<17):
+            if prod > max_product:
+                max_product = prod
+        if j < 17:
             prod = g[20*i+j]*g[20*i+j+1]*g[20*i+j+2]*g[20*i+j+3]
-            if (prod > maxproduct):
-                maxproduct = prod
-        if (i>2) and (j<17):
+            if prod > max_product:
+                max_product = prod
+        if i > 2 and j < 17:
             prod = g[20*i+j]*g[20*(i-1)+j+1]*g[20*(i-2)+j+2]*g[20*(i-3)+j+3]
-            if (prod > maxproduct):
-                maxproduct = prod
-print(maxproduct)
+            if prod > max_product:
+                max_product = prod
+print(max_product)
